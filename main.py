d6eb705faaef6152e15b350c43b74f3c1c38fb00
hwweng <hweng2@tulane.edu>
2021-08-26 10:02:05 -0500

    Implement Sum of Squares

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def sum_of_squares(a):
-	pass
+        sum = 0
+        for i in a:
+                sum = sum + i*i
+        return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+
+def test_two():
+    assert sum_of_squares([4,5,6]) == 77
+
+test_one()
+test_two()
